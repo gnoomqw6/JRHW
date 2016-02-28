b8b2f4125dd607d936b24eb2bf217d785749aa59
gnoomqw6 <gnoomqw6@gmail.com>
2016-02-28 16:25:04 +0300

    26 big 10

--- src/com/javarush/test/level26/lesson15/big01/command/WithdrawCommand.java
+++ src/com/javarush/test/level26/lesson15/big01/command/WithdrawCommand.java
@@ -1,8 +1,40 @@
 package com.javarush.test.level26.lesson15.big01.command;
 
+import com.javarush.test.level26.lesson15.big01.ConsoleHelper;
+import com.javarush.test.level26.lesson15.big01.CurrencyManipulator;
+import com.javarush.test.level26.lesson15.big01.CurrencyManipulatorFactory;
+import com.javarush.test.level26.lesson15.big01.exception.InterruptOperationException;
+import com.javarush.test.level26.lesson15.big01.exception.NotEnoughMoneyException;
+
+import java.util.Map;
+
 class WithdrawCommand implements Command{
     @Override
-    public void execute() {
+    public void execute() throws InterruptOperationException {
+        String code = ConsoleHelper.askCurrencyCode();
+        CurrencyManipulator manipulator = CurrencyManipulatorFactory.getManipulatorByCurrencyCode(code);
+        Map<Integer, Integer> result;
 
+        while (true) {
+            ConsoleHelper.writeMessage("Enter sum");
+            try {
+                int sum = Integer.parseInt(ConsoleHelper.readString());
+                if (sum <= 0) throw new NumberFormatException();
+
+                if (!manipulator.isAmountAvailable(sum)) {
+                    throw new NotEnoughMoneyException();
+                }
+                result = manipulator.withdrawAmount(sum);
+                for (Map.Entry<Integer, Integer> pair : result.entrySet()) {
+                    ConsoleHelper.writeMessage("\t" + pair.getKey() + " - " + pair.getValue());
+                }
+                ConsoleHelper.writeMessage("Success");
+                break;
+            } catch (NumberFormatException e) {
+                ConsoleHelper.writeMessage("Wrong sum");
+            } catch (NotEnoughMoneyException e) {
+                ConsoleHelper.writeMessage("Not enough money");
+            }
+        }
     }
 }
