1401bac644e45401013b704b6f34d7d376dba567
gnoomqw6 <gnoomqw6@gmail.com>
2016-03-09 22:27:21 +0300

    28 big 13

--- src/com/javarush/test/level28/lesson15/big01/Aggregator.java
+++ src/com/javarush/test/level28/lesson15/big01/Aggregator.java
@@ -1,7 +1,18 @@
 package com.javarush.test.level28.lesson15.big01;
 
+import com.javarush.test.level28.lesson15.big01.model.HHStrategy;
+import com.javarush.test.level28.lesson15.big01.model.Model;
+import com.javarush.test.level28.lesson15.big01.model.Provider;
+import com.javarush.test.level28.lesson15.big01.view.HtmlView;
+
 public class Aggregator {
     public static void main(String[] args) {
-
+        Provider[] providers = new Provider[1];
+        providers[0] = new Provider(new HHStrategy());
+        HtmlView view = new HtmlView();
+        Model model = new Model(view, providers);
+        Controller controller = new Controller(model);
+        view.setController(controller);
+        view.userCitySelectEmulationMethod();
     }
 }
--- src/com/javarush/test/level28/lesson15/big01/view/HtmlView.java
+++ src/com/javarush/test/level28/lesson15/big01/view/HtmlView.java
@@ -7,14 +7,31 @@ import java.util.List;
 
 public class HtmlView implements View {
     private Controller controller;
+    private final String filePath = this.getClass().getPackage().toString().replace('.', '/').replaceFirst("package ", "./src/") + "/vacancies.html";
 
     @Override
     public void update(List<Vacancy> vacancies) {
-
+        try {
+            updateFile(getUpdatedFileContent(vacancies));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void setController(Controller controller) {
         this.controller = controller;
+    }
+
+    public void userCitySelectEmulationMethod() {
+        controller.onCitySelect("Odessa");
+    }
+
+    private String getUpdatedFileContent(List<Vacancy> list) {
+        return null;
+    }
+
+    private void updateFile(String file) {
+
     }
 }
