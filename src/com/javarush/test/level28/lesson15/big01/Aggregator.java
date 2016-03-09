0af7b126f2b5bb0fd73ce092961671a732f750ee
gnoomqw6 <gnoomqw6@gmail.com>
2016-03-09 22:00:22 +0300

    28 big 11

--- src/com/javarush/test/level28/lesson15/big01/Aggregator.java
+++ src/com/javarush/test/level28/lesson15/big01/Aggregator.java
@@ -1,17 +1,7 @@
 package com.javarush.test.level28.lesson15.big01;
 
-import com.javarush.test.level28.lesson15.big01.model.HHStrategy;
-import com.javarush.test.level28.lesson15.big01.model.Provider;
-import com.javarush.test.level28.lesson15.big01.model.Strategy;
-import com.javarush.test.level28.lesson15.big01.vo.Vacancy;
-
-import java.util.List;
-
 public class Aggregator {
     public static void main(String[] args) {
-        Strategy strategy = new HHStrategy();
-        Provider provider = new Provider(strategy);
-        Controller controller = new Controller(provider);
-        controller.scan();
+
     }
 }
--- src/com/javarush/test/level28/lesson15/big01/Controller.java
+++ src/com/javarush/test/level28/lesson15/big01/Controller.java
@@ -1,37 +1,18 @@
 package com.javarush.test.level28.lesson15.big01;
 
-import com.javarush.test.level28.lesson15.big01.model.Provider;
-import com.javarush.test.level28.lesson15.big01.vo.Vacancy;
-
-import java.util.ArrayList;
-import java.util.Arrays;
+import com.javarush.test.level28.lesson15.big01.model.Model;
 
 public class Controller {
+    private Model model;
 
-    private Provider[] providers;
-
-    public Controller(Provider... providers) {
-        if (providers == null || providers.length == 0) {
+    public Controller(Model model) {
+        if (model == null) {
             throw new IllegalArgumentException();
         }
-        this.providers = providers;
+        this.model = model;
     }
 
-    @Override
-    public String toString() {
-        return "Controller{" + "providers=" + Arrays.toString(providers) + '}';
-    }
-
-    public void scan() {
-        ArrayList<Vacancy> vacs = new ArrayList<>();
-        try{
-            for (Provider provider:providers){
-                for (Vacancy vacancy:provider.getJavaVacancies("SOME TEXT")){
-                    vacs.add(vacancy);
-                }
-            }
-        }catch(NullPointerException e){
-        }
-        System.out.println(vacs.size());
+    public void onCitySelect(String cityName) {
+        model.selectCity(cityName);
     }
 }
