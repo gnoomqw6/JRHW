efd4d95d7ada5469defd090d247e7a9ccd4cd3ea
gnoomqw6 <gnoomqw6@gmail.com>
2016-03-09 17:34:50 +0300

    28 big 06

--- src/com/javarush/test/level28/lesson15/big01/Aggregator.java
+++ src/com/javarush/test/level28/lesson15/big01/Aggregator.java
@@ -1,5 +1,6 @@
 package com.javarush.test.level28.lesson15.big01;
 
+import com.javarush.test.level28.lesson15.big01.model.HHStrategy;
 import com.javarush.test.level28.lesson15.big01.model.Provider;
 import com.javarush.test.level28.lesson15.big01.model.Strategy;
 import com.javarush.test.level28.lesson15.big01.vo.Vacancy;
@@ -8,12 +9,8 @@ import java.util.List;
 
 public class Aggregator {
     public static void main(String[] args) {
-        Provider provider = new Provider(new Strategy() {
-            @Override
-            public List<Vacancy> getVacancies(String searchString) {
-                return null;
-            }
-        });
+        Strategy strategy = new HHStrategy();
+        Provider provider = new Provider(strategy);
         Controller controller = new Controller(provider);
         controller.scan();
     }
--- src/com/javarush/test/level28/lesson15/big01/model/HHStrategy.java
+++ src/com/javarush/test/level28/lesson15/big01/model/HHStrategy.java
@@ -1,7 +1,10 @@
 package com.javarush.test.level28.lesson15.big01.model;
 
 import com.javarush.test.level28.lesson15.big01.vo.Vacancy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
+import java.io.IOException;
 import java.util.List;
 
 public class HHStrategy implements Strategy {
@@ -9,6 +12,13 @@ public class HHStrategy implements Strategy {
 
     @Override
     public List<Vacancy> getVacancies(String searchString) {
+        String url = String.format(URL_FORMAT, "Киев", 1);
+        try {
+            Document document = Jsoup.connect(url).get();
+            System.out.println(document);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
