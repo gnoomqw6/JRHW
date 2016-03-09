69ecd60e785a40bcea12ede8ca61ca630c27411b
gnoomqw6 <gnoomqw6@gmail.com>
2016-03-09 18:38:25 +0300

    28 big 07

--- src/com/javarush/test/level28/lesson15/big01/model/HHStrategy.java
+++ src/com/javarush/test/level28/lesson15/big01/model/HHStrategy.java
@@ -9,12 +9,15 @@ import java.util.List;
 
 public class HHStrategy implements Strategy {
     private static final String URL_FORMAT = "http://hh.ua/search/vacancy?text=java+%s&page=%d";
+    private static final String USER_AGENT =
+            "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36 OPR/35.0.2066.92";
+    private static final String REFERRER = "http://google.ru";
 
     @Override
     public List<Vacancy> getVacancies(String searchString) {
         String url = String.format(URL_FORMAT, "Киев", 1);
         try {
-            Document document = Jsoup.connect(url).get();
+            Document document = Jsoup.connect(url).userAgent(USER_AGENT).referrer(REFERRER).get();
             System.out.println(document);
         } catch (IOException e) {
             e.printStackTrace();
