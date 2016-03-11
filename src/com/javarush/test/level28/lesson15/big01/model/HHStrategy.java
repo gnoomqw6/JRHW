b890ae64321a1db5e57d33a5dace1710541b9f24
gnoomqw6 <gnoomqw6@gmail.com>
2016-03-11 16:26:33 +0300

    29 big start

--- src/com/javarush/test/level28/lesson15/big01/model/HHStrategy.java
+++ src/com/javarush/test/level28/lesson15/big01/model/HHStrategy.java
@@ -1,57 +1,57 @@
-package com.javarush.test.level28.lesson15.big01.model;
-
-import com.javarush.test.level28.lesson15.big01.vo.Vacancy;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-public class HHStrategy implements Strategy {
-    private static final String URL_FORMAT = "http://hh.ua/search/vacancy?text=java+%s&page=%d";
-    private static final String USER_AGENT =
-            "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36 OPR/35.0.2066.92";
-    private static final String REFERRER = "http://google.ru";
-
-    @Override
-    public List<Vacancy> getVacancies(String searchString) {
-        List<Vacancy> vacancyList = new ArrayList<>();
-        int i = 0;
-        Document document = null;
-        while (true) {
-            try {
-                document = getDocument(searchString, i++);
-            } catch (IOException e) {
-            }
-
-            Elements all = document.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy");
-
-            if (all.size() == 0) {
-                break;
-            }
-
-            for (Element element : all) {
-                Vacancy vacancy = new Vacancy();
-                vacancy.setUrl(element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy-title").attr("href"));
-                vacancy.setTitle(element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy-title").text());
-                vacancy.setSalary(element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy-compensation").text());
-                vacancy.setCity(element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy-address").text());
-                vacancy.setCompanyName(element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy-employer").text());
-                vacancy.setSiteName(document.title());
-                vacancyList.add(vacancy);
-            }
-        }
-
-        return vacancyList;
-    }
-
-    protected Document getDocument(String searchString, int page) throws IOException{
-        String url = String.format(URL_FORMAT, searchString, page);
-        Document document = Jsoup.connect(url).userAgent(USER_AGENT).referrer(REFERRER).get();
-        System.out.println(document);
-        return document;
-    }
-}
+//package com.javarush.test.level28.lesson15.big01.model;
+//
+//import com.javarush.test.level28.lesson15.big01.vo.Vacancy;
+//import org.jsoup.Jsoup;
+//import org.jsoup.nodes.Document;
+//import org.jsoup.nodes.Element;
+//import org.jsoup.select.Elements;
+//
+//import java.io.IOException;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class HHStrategy implements Strategy {
+//    private static final String URL_FORMAT = "http://hh.ua/search/vacancy?text=java+%s&page=%d";
+//    private static final String USER_AGENT =
+//            "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36 OPR/35.0.2066.92";
+//    private static final String REFERRER = "http://google.ru";
+//
+//    @Override
+//    public List<Vacancy> getVacancies(String searchString) {
+//        List<Vacancy> vacancyList = new ArrayList<>();
+//        int i = 0;
+//        Document document = null;
+//        while (true) {
+//            try {
+//                document = getDocument(searchString, i++);
+//            } catch (IOException e) {
+//            }
+//
+//            Elements all = document.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy");
+//
+//            if (all.size() == 0) {
+//                break;
+//            }
+//
+//            for (Element element : all) {
+//                Vacancy vacancy = new Vacancy();
+//                vacancy.setUrl(element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy-title").attr("href"));
+//                vacancy.setTitle(element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy-title").text());
+//                vacancy.setSalary(element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy-compensation").text());
+//                vacancy.setCity(element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy-address").text());
+//                vacancy.setCompanyName(element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy-employer").text());
+//                vacancy.setSiteName(document.title());
+//                vacancyList.add(vacancy);
+//            }
+//        }
+//
+//        return vacancyList;
+//    }
+//
+//    protected Document getDocument(String searchString, int page) throws IOException{
+//        String url = String.format(URL_FORMAT, searchString, page);
+//        Document document = Jsoup.connect(url).userAgent(USER_AGENT).referrer(REFERRER).get();
+//        System.out.println(document);
+//        return document;
+//    }
+//}
--- src/com/javarush/test/level28/lesson15/big01/view/HtmlView.java
+++ src/com/javarush/test/level28/lesson15/big01/view/HtmlView.java
@@ -1,69 +1,69 @@
-package com.javarush.test.level28.lesson15.big01.view;
-
-import com.javarush.test.level28.lesson15.big01.Controller;
-import com.javarush.test.level28.lesson15.big01.vo.Vacancy;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-
-import java.io.*;
-import java.util.List;
-
-public class HtmlView implements View {
-    private Controller controller;
-    private final String filePath = this.getClass().getPackage().toString().replace('.', '/').replaceFirst("package ", "./src/") + "/vacancies.html";
-
-    @Override
-    public void update(List<Vacancy> vacancies) {
-        try {
-            updateFile(getUpdatedFileContent(vacancies));
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Some exception occurred");
-        }
-    }
-
-    @Override
-    public void setController(Controller controller) {
-        this.controller = controller;
-    }
-
-    public void userCitySelectEmulationMethod() {
-        controller.onCitySelect("Odessa");
-    }
-
-    private String getUpdatedFileContent(List<Vacancy> list) throws IOException {
-        Document document = getDocument();
-
-        Element template = document.getElementsByClass("template").first();
-
-        Element cloneTemp = template.clone();
-        cloneTemp.removeAttr("style");
-        cloneTemp.removeClass("template");
-
-        document.select("tr[class=vacancy]").remove();
-
-        for (Vacancy vacancy : list) {
-            Element clon = cloneTemp.clone();
-            clon.getElementsByClass("city").get(0).text(vacancy.getCity());
-            clon.getElementsByClass("companyName").get(0).text(vacancy.getCompanyName());
-            clon.getElementsByClass("salary").get(0).text(vacancy.getSalary());
-            Element link = clon.getElementsByTag("a").first();
-            link.text(vacancy.getTitle());
-            link.attr("href", vacancy.getUrl());
-            template.before(clon.outerHtml());
-        }
-
-        return document.html();
-    }
-
-    private void updateFile(String file) throws FileNotFoundException, UnsupportedEncodingException {
-        PrintWriter pw = new PrintWriter(new OutputStreamWriter(new FileOutputStream(filePath), "UTF-8"));
-        pw.print(file);
-        pw.close();
-    }
-
-    protected Document getDocument() throws IOException {
-        return Jsoup.parse(new File(filePath), "UTF-8");
-    }
-}
+//package com.javarush.test.level28.lesson15.big01.view;
+//
+//import com.javarush.test.level28.lesson15.big01.Controller;
+//import com.javarush.test.level28.lesson15.big01.vo.Vacancy;
+//import org.jsoup.Jsoup;
+//import org.jsoup.nodes.Document;
+//import org.jsoup.nodes.Element;
+//
+//import java.io.*;
+//import java.util.List;
+//
+//public class HtmlView implements View {
+//    private Controller controller;
+//    private final String filePath = this.getClass().getPackage().toString().replace('.', '/').replaceFirst("package ", "./src/") + "/vacancies.html";
+//
+//    @Override
+//    public void update(List<Vacancy> vacancies) {
+//        try {
+//            updateFile(getUpdatedFileContent(vacancies));
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            System.out.println("Some exception occurred");
+//        }
+//    }
+//
+//    @Override
+//    public void setController(Controller controller) {
+//        this.controller = controller;
+//    }
+//
+//    public void userCitySelectEmulationMethod() {
+//        controller.onCitySelect("Odessa");
+//    }
+//
+//    private String getUpdatedFileContent(List<Vacancy> list) throws IOException {
+//        Document document = getDocument();
+//
+//        Element template = document.getElementsByClass("template").first();
+//
+//        Element cloneTemp = template.clone();
+//        cloneTemp.removeAttr("style");
+//        cloneTemp.removeClass("template");
+//
+//        document.select("tr[class=vacancy]").remove();
+//
+//        for (Vacancy vacancy : list) {
+//            Element clon = cloneTemp.clone();
+//            clon.getElementsByClass("city").get(0).text(vacancy.getCity());
+//            clon.getElementsByClass("companyName").get(0).text(vacancy.getCompanyName());
+//            clon.getElementsByClass("salary").get(0).text(vacancy.getSalary());
+//            Element link = clon.getElementsByTag("a").first();
+//            link.text(vacancy.getTitle());
+//            link.attr("href", vacancy.getUrl());
+//            template.before(clon.outerHtml());
+//        }
+//
+//        return document.html();
+//    }
+//
+//    private void updateFile(String file) throws FileNotFoundException, UnsupportedEncodingException {
+//        PrintWriter pw = new PrintWriter(new OutputStreamWriter(new FileOutputStream(filePath), "UTF-8"));
+//        pw.print(file);
+//        pw.close();
+//    }
+//
+//    protected Document getDocument() throws IOException {
+//        return Jsoup.parse(new File(filePath), "UTF-8");
+//    }
+//}
