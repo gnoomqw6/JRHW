8fd783a7d772d9bb215bd9f2a4efed178a3d8635
gnoomqw6 <gnoomqw6@gmail.com>
2016-03-13 17:04:12 +0300

    29 big 12

--- src/com/javarush/test/level29/lesson15/big01/car/Car.java
+++ src/com/javarush/test/level29/lesson15/big01/car/Car.java
@@ -2,7 +2,7 @@ package com.javarush.test.level29.lesson15.big01.car;
 
 import java.util.Date;
 
-public class Car {
+public abstract class Car {
     static public final int TRUCK = 0;
     static public final int SEDAN = 1;
     static public final int CABRIOLET = 2;
@@ -56,10 +56,9 @@ public class Car {
     }
 
     public int getNumberOfPassengersCanBeTransferred() {
-        if (!isDriverAvailable())
+        if (!canPassengersBeTransferred()) {
             return 0;
-        if (fuel <= 0)
-            return 0;
+        }
 
         return numberOfPassengers;
     }
@@ -75,10 +74,8 @@ public class Car {
     public void startMoving() {
         if (numberOfPassengers > 0) {
             fastenPassengersBelts();
-            fastenDriverBelt();
-        } else {
-            fastenDriverBelt();
         }
+        fastenDriverBelt();
     }
 
     public void fastenPassengersBelts() {
@@ -87,13 +84,7 @@ public class Car {
     public void fastenDriverBelt() {
     }
 
-    public int getMaxSpeed() {
-        if (type == TRUCK)
-            return 80;
-        if (type == SEDAN)
-            return 120;
-        return 90;
-    }
+    public abstract int getMaxSpeed();
 
     public boolean isSummer(Date date , Date summerStart, Date summerEnd) {
         return (date.after(summerStart) && date.before(summerEnd));
@@ -105,5 +96,9 @@ public class Car {
 
     public double getSummerConsumption(int length) {
         return summerFuelConsumption * length;
+    }
+
+    private boolean canPassengersBeTransferred() {
+        return (isDriverAvailable() && fuel > 0);
     }
 }
