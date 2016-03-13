c01a49bd905d1379cacea24889815d24cc341728
gnoomqw6 <gnoomqw6@gmail.com>
2016-03-13 17:38:10 +0300

    29 big 14

--- src/com/javarush/test/level29/lesson15/big01/user/User.java
+++ src/com/javarush/test/level29/lesson15/big01/user/User.java
@@ -4,11 +4,9 @@ public class User {
     private String name;
     private String surname;
     private int age;
+    private boolean man;
 
-    private String country;
-    private String city;
-    private House house;
-
+    private Address address;
     private Work work;
 
     public User(String name, String surname, int age) {
@@ -41,24 +39,16 @@ public class User {
         this.age = age;
     }
 
-    public String getCountry() {
-        return country;
+    public boolean isMan() {
+        return man;
     }
 
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
+    public void setMan(boolean man) {
+        this.man = man;
     }
 
     public String getAddress() {
-        return country + " " + city + " " + house.house;
+        return address.getCountry() + " " + address.getCity() + " " + address.getHouse();
     }
 
     public Work getWork() {
@@ -67,6 +57,10 @@ public class User {
 
     public void setWork(Work work) {
         this.work = work;
+    }
+
+    public String getBoss() {
+        return work.getBoss();
     }
 
     public void printInfo() {
