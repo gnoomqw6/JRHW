041111c014a72bf8c21224804077e4fab1ed22e8
gnoomqw6 <gnoomqw6@gmail.com>
2016-03-13 15:34:19 +0300

    29 big 09

--- src/com/javarush/test/level29/lesson15/big01/human/Human.java
+++ src/com/javarush/test/level29/lesson15/big01/human/Human.java
@@ -11,18 +11,19 @@ public class Human implements Alive{
     protected int age;
     protected String name;
 
-    protected int[] size;
+    protected Size size;
 
     public static final int FIRST = 1;
     public static final int SECOND = 2;
     public static final int THIRD = 3;
     public static final int FOURTH = 4;
-    private int bloodGroup;
+    private BloodGroup bloodGroup;
 
-    public void setBloodGroup(int code) {
-        bloodGroup = code;
+    public void setBloodGroup(BloodGroup bloodGroup) {
+        this.bloodGroup = bloodGroup;
     }
-    public int getBloodGroup() {
+
+    public BloodGroup getBloodGroup() {
         return bloodGroup;
     }
 
@@ -70,7 +71,7 @@ public class Human implements Alive{
     }
 
     public void printSize() {
-        System.out.println("Рост: " + size[0] + " Вес: " + size[1]);
+        System.out.println("Рост: " + size.height + " Вес: " + size.weight);
     }
 
     public String getPosition() {
@@ -79,5 +80,10 @@ public class Human implements Alive{
 
     public void printData() {
         System.out.println(getPosition() + ": " + name);
+    }
+
+    public class Size {
+        public int height;
+        public int weight;
     }
 }
