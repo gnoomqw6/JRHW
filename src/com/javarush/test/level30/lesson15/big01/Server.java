19b9e0a9336c6128745fb4e5627dc210ffaa597c
gnoomqw6 <gnoomqw6@gmail.com>
2016-03-14 12:42:13 +0300

    30 big 06

--- src/com/javarush/test/level30/lesson15/big01/Server.java
+++ src/com/javarush/test/level30/lesson15/big01/Server.java
@@ -1,4 +1,34 @@
 package com.javarush.test.level30.lesson15.big01;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(ConsoleHelper.readInt());
+            ConsoleHelper.writeMessage("Server started");
+            while (true) {
+                Socket socket = serverSocket.accept();
+                Handler handler = new Handler(socket);
+                handler.start();
+            }
+        } catch (Exception e) {
+            try {
+                serverSocket.close();
+            } catch (IOException e1) {
+            }
+            ConsoleHelper.writeMessage("Server exception, serverSocket closed");
+        }
+    }
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+    }
 }
