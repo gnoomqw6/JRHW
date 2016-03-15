f699859fef43d8e19cadadfcc16d708622c54e58
gnoomqw6 <gnoomqw6@gmail.com>
2016-03-15 17:55:26 +0300

    31 big 04

--- src/com/javarush/test/level31/lesson15/big01/Archiver.java
+++ src/com/javarush/test/level31/lesson15/big01/Archiver.java
@@ -1,12 +1,14 @@
 package com.javarush.test.level31.lesson15.big01;
 
+import com.javarush.test.level31.lesson15.big01.command.ExitCommand;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Paths;
 
 public class Archiver {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             System.out.println("Enter full path and name of NEW zip file");
             String pathToZip = reader.readLine();
@@ -20,5 +22,7 @@ public class Archiver {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        new ExitCommand().execute();
     }
 }
