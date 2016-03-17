62b0b40dd54b869a8bc220f2cab71355cd32114f
gnoomqw6 <gnoomqw6@gmail.com>
2016-03-17 13:37:47 +0300

    32 big 02

--- src/com/javarush/test/level32/lesson15/big01/Controller.java
+++ src/com/javarush/test/level32/lesson15/big01/Controller.java
@@ -8,7 +8,23 @@ public class Controller {
     private HTMLDocument document;
     private File currentFile;
 
+    public static void main(String[] args) {
+        View view = new View();
+        Controller controller = new Controller(view);
+        view.setController(controller);
+        view.init();
+        controller.init();
+    }
+
     public Controller(View view) {
         this.view = view;
+    }
+
+    public void init() {
+
+    }
+
+    public void exit() {
+        System.exit(0);
     }
 }
